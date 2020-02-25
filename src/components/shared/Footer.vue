c9ab53d0b61e5439f53e60f78173b733b0bcefcd
myjade <myjade999@gmail.com>
2020-02-25 16:41:11 +0900

    footer

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,30 +1,21 @@
 <template>
-<footer id="swsFoot" class="common-footer-item on-improvement">
-    <small class="copr_info">
-        <a href="javascript:goUrl('open', '');" class="link_swscorp" role="button"><span class="img_txt txt_copy">© shallweshop</span></a>
-    </small>
+<footer id="swsFoot">
     <ul class="list_foot">
-        <li>
-            <a class="btn_foot" href="/agreement" data-title="이용약관">
-                이용약관
-            </a>
-        </li>
-        <li>
-            <a class="btn_foot" href="kakaotalk://buy/about/4/terms" data-title="쉘위샵 이용약관">
-                쉘위샵 이용약관
-            </a>
-        </li>
-        <li>
-            <a class="btn_foot" href="/biz" data-title="사업자정보">
-                사업자정보
-            </a>
-        </li>
-                <li>
-            <a class="btn_foot" href="/">
-                파트너센터
-            </a>
-        </li>
-            </ul>
+        <li><a class="btn_foot" href="/agreement" data-title="회사소개">회사소개</a></li>
+        <li><a class="btn_foot" href="/about/4/terms" data-title="이용약관">이용약관</a></li>
+        <li><a class="btn_foot" href="/biz" data-title="개인정보처리방침">개인정보처리방침</a></li>
+        <li><a class="btn_foot" href="/" data-title="고객센터">고객센터</a></li>
+    </ul>
+    <ul class="info_foot">
+        <li class="corp_name">(주)이피엔스</li>
+        <li><span>사업자 등록번호</span>642-81-01535</li>
+        <li><span>대표이사</span>이현승</li>
+        <li><span>주소</span><span class="addr">서울 강남구 봉은사로 51길 19(논현동, 알디에스사옥)1층</span></li>
+        <li><span>전화번호</span>070-7747-0059</li>
+    </ul>
+     <div class="copr_info">
+        <a href="/" class="link_swscorp" role="button">© 2020 ShallWeShop, Inc.All right reserved.</a>
+    </div>
 </footer>
 </template>
 
