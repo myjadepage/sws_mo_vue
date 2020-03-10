94f1d5b9ada04d6cb9c9cf9c561929b9a4ce8a4e
Gonhong <sarkh91@epiens.com>
2020-03-10 21:00:50 +0900

    update product footer

--- src/components/product/Footer/ProductFooterOption.vue
+++ src/components/product/Footer/ProductFooterOption.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="productOptionWrap" v-if="false">
-    <select name="prdtNormalOptionSysId" id="prdtNormalOptionSysId">
-      <option value="">옵션선택</option>
-      <option v-for="(o,index) in product.normalOptions" :key="index" :value="o.prdtNormalOptionSysId">{{`0${index+1}. ${o.content}`}}</option>
+    <select @select="optionSelected" v-for="(o,idx) in product.normalOptions" :key="idx" ref="option">
+      <option value="">{{o.name}}</option>
+      <option v-for="(c, idx) in o.content" :key="idx" :value="c" >{{`0${idx+1}. ${c}`}}</option>
     </select>
+    <div class="selectedOption">
+
+    </div>
   </div>
 </template>
 
@@ -12,9 +15,23 @@ export default {
   data () {
     return {
       product: {'normalOptions': [
-        {'prdtNormalOptionSysId': 1, 'name': '사이즈', 'content': '95, 100, 105 사이즈 판매합니다.'},
-        {'prdtNormalOptionSysId': 2, 'name': '색상', 'content': '흰색, 핑크색, 노랜색, 검정색 판매합니다.'}
+        {
+          name: '사이즈',
+          content: [90, 95, 100],
+          price: 29900
+        },
+        {
+          name: '색상',
+          content: ['흰색', '핑크색', '노랑', '검정'],
+          price: 0
+        }
       ]}
+    }
+  },
+  methods: {
+    optionSelected () {
+      let option = this.$refs.option
+      console.log(option.values)
     }
   }
 }
@@ -24,5 +41,12 @@ export default {
 .productOptionWrap{
     height: 250px;
     text-align: center;
+}
+
+.productOptionWrap select{
+  padding: 10px;
+  border: 1px solid #eeeeee;
+  width: 90%;
+  margin-bottom: 10px;
 }
 </style>
