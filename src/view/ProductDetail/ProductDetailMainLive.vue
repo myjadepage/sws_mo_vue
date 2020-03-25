7fea111d67f7ba5fe340d832578b4298df500a99
Gonhong <sarkh91@epiens.com>
2020-03-25 17:36:36 +0900

    temp

---
+++ src/view/ProductDetail/ProductDetailMainLive.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="productDetailWrap" v-if="product.prdtSysId">
+      <MediaLive/>
+  </div>
+</template>
+
+<script>
+import MediaLive from '@/components/product/ProductMediaLive'
+import { getProduct } from '@/api/index'
+
+export default {
+  created () {
+    let id = this.$route.params.prdtSysId
+    let self = this
+
+    getProduct(id).then((res) => {
+      self.$store.state.product = res.data.jsonData.product
+      self.product = res.data.jsonData.product
+    }
+    ).catch((e) => console.log(e)
+    )
+  },
+  data () {
+    return {
+      title: '상품상세',
+      product: {}
+    }
+  },
+  components: {
+    MediaLive
+  }
+}
+</script>
+
+<style>
+.productDetailWrap{
+  min-width: 360px;
+}
+</style>
