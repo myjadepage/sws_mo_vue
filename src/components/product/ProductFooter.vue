f8009b4dcbbdf91194d2d08c10d11eb14202338b
myjade <myjade999@gmail.com>
2020-04-06 10:33:08 +0900

    충돌충돌

--- src/components/product/ProductFooter.vue
+++ src/components/product/ProductFooter.vue
@@ -11,12 +11,8 @@
 
 <script>
 import Option from './Footer/ProductFooterOption'
-<<<<<<< HEAD
-import { postCartItem, getAccessToken } from '@/api/index.js'
-=======
 import {postCartItem, getAccessToken} from '@/api/index.js'
 // import {isEquals} from '@/assets/js/common.js'
->>>>>>> swsmo2/master
 
 export default {
   props: ['buyMode', 'options'],
