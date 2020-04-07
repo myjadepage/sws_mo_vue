fe3e3bd957fe68e769cdfc2ea22e2657fe67cfdd
skftpwk22 <skftpwk@gmail.com>
2020-04-07 21:17:16 +0900

    마이페이지 충돌 수정

--- src/view/mypage/MyReview.vue
+++ src/view/mypage/MyReview.vue
@@ -20,6 +20,8 @@ import {getMyReviewList, getAccessToken} from '@/api/index.js'
 
 export default {
   created () {
+    window.scrollTo(0, 0)
+
     getMyReviewList(sessionStorage.getItem('accessToken'))
       .then(res => {
         if (res.data.jsonData.reviews) {
@@ -89,10 +91,6 @@ export default {
       title: '마이리뷰',
       pageType: 'review',
       emptyMessage: '리뷰를 작성하지 않은 상품 내역이 없습니다.',
-<<<<<<< HEAD
-      reviews: [],
-      products: [],
-=======
       emptyMessage2: '리뷰를 작성한 상품 내역이 없습니다.',
       products: [],
       reviews: {
@@ -103,15 +101,11 @@ export default {
           length: 24
         }
       },
->>>>>>> a4d10c7fd287d51e7428f0ae46d06773574466e7
       tabState: false
     }
   },
   components: {
     Bar, ExchangeList, EmptyBlock, ReviewTab, ReviewEntity
-  },
-  created () {
-    window.scrollTo(0, 0)
   },
   methods: {
     tabSwitch () {
